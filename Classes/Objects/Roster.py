039258b472c20bccef63de310230db120a17a91e
Manasa <m.neruganti@gmail.com>
2025-06-14 23:03:17 -0400

    Added GPA attribute with getters and setters

--- Classes/Objects/Roster.py
+++ Classes/Objects/Roster.py
@@ -2,14 +2,17 @@ from Student import Student
 
 class Roster:
     
-    student_one = Student("Amy","Jones",14,"009876")
-    student_two = Student("Kevin","Jones",14,"008765")
-    student_three = Student("Lisa","James",15,"001245")
-    student_four = Student("Theo","Khan",14,"003674")
-    student_five = Student("Joseph","Shetz",16,"002386")
+    student_one = Student("Amy","Jones",14,"009876", 3.8)
+    student_two = Student("Kevin","Jones",14,"008765", 3.9)
+    student_three = Student("Lisa","James",15,"001245", 3.7)
+    student_four = Student("Theo","Khan",14,"003674", 3.8)
+    student_five = Student("Joseph","Shetz",16,"002386", 3.2)
     
     print(f"Student One:\n{student_one}")
     print(f"Student Two:\n{student_two}")
     print(f"Student Three:\n{student_three}")
     print(f"Student Four:\n{student_four}")
     print(f"Student Five:\n{student_five}")
+    
+    student_one.setGPA(3.4)
+    print(f"Student One has one change in GPA: {student_one.getGPA()}\n")
--- Classes/Objects/Student.py
+++ Classes/Objects/Student.py
@@ -3,16 +3,25 @@ class Student:
     # The init function is called automatically everytime a class is used to create an object
     # I initilize this init function to take in a first name, last name, age, and id
     # and then assign the given values to the object as its properties
-    def __init__(self, fname, lname, age, id):
+    def __init__(self, fname, lname, age, id, gpa):
         self.fname = fname
         self.lname = lname
         self.age = age
         self.id = id
+        self.gpa = gpa
+    
+    # A setter method for the students GPA to update it if necessary
+    def setGPA(self, gpa):
+        self.gpa = gpa
+    
+    # A getter method to return the student gpa when needed
+    def getGPA(self):
+        return self.gpa
         
     # The str function is essentially the toString and displays how the object will look
     # when it is called in a print statement. Otherwise, it will return a default object string
     def __str__(self):
-        return f"Name: {self.fname} {self.lname}\nAge: {self.age}\nID: {self.id}\n"
+        return f"Name: {self.fname} {self.lname}\nAge: {self.age}\nID: {self.id}\nGPA: {self.gpa}\n"
     
     # the parameter self references the current instance of the class and can be used
     # to access the variables of the class
